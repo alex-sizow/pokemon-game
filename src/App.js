6c9a148518ce7d1059e2ba2fd8741374bd22fa14
alex-sizow <alex_sizow@outlook.com>
2021-01-28 21:51:13 +0300

    second com

--- src/App.js
+++ src/App.js
@@ -1,37 +1,31 @@
-import HeaderBlock from './components/HeaderBlock/index';
-
+import Header from './components/Header/index';
+import Layout from './components/Layout/index';
+import Footer from './components/Footer/index';
 import './App.css';
-
-const AppList = () => {
-	const items = ['Item1', 'Item2', 'Item3', 'Item4', 'Item5'];
-	const firstItems = <li>Item0</li>;
-	return (
-		<ul>
-			{firstItems}
-			{items.map((item) => (
-				<li>{item + '!'}</li>
-			))}
-		</ul>
-	);
-};
-
-const AppHeader = () => {
-	return <h1 className='header'>This is test!</h1>;
-};
-
-const AppInput = () => {
-	const placeholder = 'Type text...';
-	return (
-		<label>
-			<input placeholder={placeholder} />
-		</label>
-	);
-};
 
 const App = () => {
 	return (
 		<div className='App'>
-			<HeaderBlock title='This is new Title' />
+			<Header
+				title='This is title'
+				descr='This is Description!'
+			/>
+			<Layout
+				id='1'
+				title='Передаю title'
+				desc='Передал еще desc'
+			/>
+			<Layout
+				id='2'
+				title='Передаю title'
+				desc='Передал еще desc'
+			/>
+			<Layout
+				id='3'
+				title='Передаю title'
+				desc='Передал еще desc'
+			/>
+			<Footer />
 		</div>
 	);
 };
